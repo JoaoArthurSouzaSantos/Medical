ca2a494768010118eebe57e2ae9e54a232c75998
João Arthur Souza Santos <joaoarthursouzasantos@gmail.com>
2024-05-21 08:43:00 -0300

    feat(database): Adiciona migração para entidade Atendimento

---
+++ database/migrations/2024_05_21_114151_create_atendimentos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtendimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atendimentos', function (Blueprint $table) {
+            $table->id();
+            $table->date('data_atendimento');
+            $table->foreignId('medico_id')->constrained('medicos');
+            $table->foreignId('paciente_id')->constrained('pacientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atendimentos');
+    }
+}
